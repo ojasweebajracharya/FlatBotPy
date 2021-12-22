3833d419765839f083aca8110d0428377a21b88a
Ojaswee Bajracharya <s2015068@ed.ac.uk>
2021-12-22 21:20:26 +0000

    added code from our previous repl as a comment

--- bot.py
+++ bot.py
@@ -14,6 +14,53 @@ async def on_message(message):
         return
     else:
         await message.channel.send("Hello there!")
-    
-    
+
 client.run(os.environ['TOKEN'])
+
+# this was what was on repl
+# import os
+# import aiocron
+# import discord
+# from dotenv import load_dotenv
+
+# load_dotenv()
+# TOKEN = os.getenv('TOKEN')
+
+# client = discord.Client()
+
+# @client.event
+# async def on_ready():
+#     print(f'{client.user} has connected to Discord!')
+
+# flatmates = ["Simran","Ojaswee","Emily","Fraser"]
+# num = 0
+
+# def runBot():
+#   client.on("ready", printSchedule())
+
+# def printSchedule():
+#   flatBotChannel = client.channels.cache.get("895388658885095465")
+  
+#   flatBotChannel.send("Hiiiii! This week it is "+ flatmates[num] + "'s turn to take out the kitchen bins and vacuum the corridor and mop (if needed). ")
+
+#   flatBotChannel.send(flatmates[num+1] + "'s turn to clean the bathroom with the shower (clean shower, wipe all surfaces, mop floor? (vacuum? if the floor is dry?))")
+
+#   flatBotChannel.send(flatmates[num+2] + "'s turn to clean the smaller bathroom - clean all surfaces, mop floor? vacuum?")
+
+#   flatBotChannel.send(flatmates[num+3] + "'s turn to clean the kitchen and sofa areas. This includes vacuuming the floor, mopping, cleaning all surfaces which includes sink, hob, fridge etc.")
+
+#   if (num == 3):
+#     num = 0;
+#     runBot()
+#   else:
+#     num += 1;
+#     runBot()
+
+# @aiocron.crontab('0 * * * *')
+# async def cornjob1():
+#     await flatBotChannel.send('Hour Cron Test')
+
+
+
+# client.run(TOKEN)
+
