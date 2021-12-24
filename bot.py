16f11f1f65532283512cf7155708ea167bfced30
Ojaswee Bajracharya <s2015068@ed.ac.uk>
2021-12-24 07:14:29 +0000

    send schedule on every monday

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 client = discord.Client()
-
+flatBotChannel = client.get_channel(634765417574957078)
 flatmates = ["Simran","Ojaswee","Emily","Fraser"]
 num = 0
 
@@ -27,14 +27,12 @@ num = 0
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-    await testChannel()
     
 
-def runBot():
-  client.on("ready", testChannel())
+# def runBot():
+#   client.on("ready", testChannel())
 
 def printSchedule():
-  flatBotChannel = client.channels.cache.get("634765417574957078")
   flatBotChannel.send("Hiiiii! This week it is "+ flatmates[num] + "'s turn to take out the kitchen bins and vacuum the corridor and mop (if needed). ")
 
   flatBotChannel.send(flatmates[num+1] + "'s turn to clean the bathroom with the shower (clean shower, wipe all surfaces, mop floor? (vacuum? if the floor is dry?))")
@@ -45,19 +43,18 @@ def printSchedule():
 
   if (num == 3):
     num = 0
-    runBot()
   else:
     num += 1
-    runBot()
 
-@aiocron.crontab('0 * * * *')
+@aiocron.crontab('0 6 * * mon')
 async def cornjob1():
-    await client.get_channel(634765417574957078).send('Hour Cron Test')
+    await printSchedule()
 
 
-async def testChannel():
-  channel = client.get_channel(634765417574957078)
-  await channel.send('hello')
+
+# async def testChannel():
+#   channel = client.get_channel(634765417574957078)
+#   await channel.send('hello')
 
 # client.run(os.environ['TOKEN'])
 
