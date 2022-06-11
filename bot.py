245ceb78e7af68748a99b24398d3a378e4caaebd
Ojaswee Bajracharya <s2015068@ed.ac.uk>
2022-06-11 16:58:34 +0100

    including mentiosn in messages

--- bot.py
+++ bot.py
@@ -20,13 +20,18 @@ collection = db["globalvars"]
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 client = discord.Client()
-flatmates = ["Emily","Simran","Ojaswee"]
+oj_id = "571276422363217951"
+em_id = "238389040187965441"
+sim_id = "719261320662351950"
+flatmates_ids = [em_id, sim_id, oj_id]
 
 async def mentioning_User():
   flatBotChannel = client.get_channel(634765417574957078)
   print(client.users)
-  user = "571276422363217951"
-  await flatBotChannel.send(f"<@{user}> is the best")
+  oj_id = "571276422363217951"
+  em_id = "238389040187965441"
+  sim_id = "719261320662351950"
+  await flatBotChannel.send(f"<@{oj_id}> is the best")
 
 def update_num():
   collection.update_one({"_id":0},{ "$inc": {"num": +1}})
@@ -54,8 +59,9 @@ async def on_ready():
 # def runBot():
 #   client.on("ready", testChannel())
 
+
 async def printSchedule():
-  global flatmates  
+  global flatmates_ids  
 
   results = collection.find({"_id":0})
   numArr = [result["num"] for result in results]
@@ -63,18 +69,23 @@ async def printSchedule():
 
   flatBotChannel = client.get_channel(634765417574957078)
   
-  await flatBotChannel.send("Hiiiii! This week it is "+ flatmates[num % 3] + "'s turn to take out the kitchen bins and vacuum/broom the corridor & floors.")
+  await flatBotChannel.send(f"Hiiiii! This week it is <@{flatmates_ids[num % 3]}>'s turn to take out the kitchen bins and vacuum/broom the corridor & floors.")
 
-  await flatBotChannel.send(flatmates[(num+1) % 3] + "'s turn to clean the toilet and shower - wipe down surfaces, clean the shower :)) ")
+  await flatBotChannel.send(f"<@{flatmates_ids[(num+1) % 3]}>'s turn to clean the toilet and shower - wipe down surfaces, clean the shower :)) ")
 
-  await flatBotChannel.send(flatmates[(num+2) % 3] + "'s turn to clean the kitchen. This includes cleaning the surfaces, the hob, the microwave (inside too), the fridge (inside as well).")
+  await flatBotChannel.send(f"<@{flatmates_ids[(num+2) % 3]}>'s turn to clean the kitchen. This includes cleaning the surfaces, the hob, the microwave (inside too), the fridge (inside as well).")
 
   update_num()
 
-# @aiocron.crontab('0 0 * * mon,wed,fri,sun')
-@aiocron.crontab('0 0 * * mon')
+@aiocron.crontab('10 * * * *')
 async def cornjob1():
     await printSchedule()
+
+
+# @aiocron.crontab('0 0 * * mon,wed,fri,sun')
+# @aiocron.crontab('0 0 * * mon')
+# async def cornjob1():
+#     await printSchedule()
 
 client.run(TOKEN)
 
