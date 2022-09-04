ee4a924a422db5afac23fe7db2d3c331349fce89
Ojaswee Bajracharya <s2015068@ed.ac.uk>
2022-09-04 21:16:23 +0100

    trying to update google sheets

--- bot.py
+++ bot.py
@@ -58,7 +58,7 @@ def get_person(person_id):
 
 def get_next_free_row_number(starting_letter):
   for i in range(4, 100):
-    if wks.acell(f"{starting_letter}{i}").value == "":
+    if wks.acell(f"{starting_letter}{i}").value == None:
       print(i)
       return int(i)
   
