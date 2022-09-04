8497404aaee4ae5bbb12878f075d7b275b92bac2
Ojaswee Bajracharya <s2015068@ed.ac.uk>
2022-09-04 16:55:24 +0100

    changed google sheet link vars to global

--- bot.py
+++ bot.py
@@ -87,8 +87,8 @@ async def money(ctx, *, person = None):
 # updates people and communal (person = communal if it was communal)
 @client.command(aliases=['money-update'])
 async def moneyupdate(ctx, *args):
-  arguments = ', '.join(args)
-  await ctx.send(f'{len(args)} arguments: {arguments}')
+  # arguments = ', '.join(args)
+  # await ctx.send(f'{len(args)} arguments: {arguments}')
 
   item = args[0]
   amount = args[1]
