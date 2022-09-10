f3ba1742d4a0ada025dfd8f49f5c68e17caec6e2
Ojaswee Bajracharya <s2015068@ed.ac.uk>
2022-09-10 20:51:27 +0100

    changed bot to bot instead of client

--- bot.py
+++ bot.py
@@ -211,8 +211,20 @@ async def printSchedule():
 # @aiocron.crontab('0 0 * * mon')
 @aiocron.crontab('*/5 * * * *')
 async def cornjobSchedule():
-    print("TEST 4")
-    await printSchedule()
+  print("TEST 3")
+  results = collection.find({"_id":0})
+  numArr = [result["num"] for result in results]
+  num = numArr[0]
+
+  flatBotChannel = client.get_channel(981536894867345418)
+  
+  await flatBotChannel.send(f"Hiiiii! This week it is <@{flatmates_ids[num % 3]}>'s turn to take out the kitchen bins and vacuum/broom the hall")
+
+  await flatBotChannel.send(f"<@{flatmates_ids[(num+1) % 3]}>'s turn to clean the toilet and shower - wipe down surfaces, clean the floor, clean the shower :)) ")
+
+  await flatBotChannel.send(f"<@{flatmates_ids[(num+2) % 3]}>'s turn to clean the kitchen. This includes cleaning the surfaces, sweep the floor and use floor wipes for any spillss etc. clean the hob, the microwave (inside too), the fridge (inside as well).")
+
+
 
     # update_num()
 
